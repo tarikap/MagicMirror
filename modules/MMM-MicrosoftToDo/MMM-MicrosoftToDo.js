993a49784146eeb87c3785fc529d3b09a2af0d28
tarikap <34628342+tarikap@users.noreply.github.com>
2020-12-06 23:51:56 +0100

    Add files via upload

---
+++ modules/MMM-MicrosoftToDo/MMM-MicrosoftToDo.js
@@ -0,0 +1,157 @@
+
+/*
+global Module, Log, moment
+*/
+Module.register('MMM-MicrosoftToDo', {
+
+  // Override dom generator.
+  getDom: function () {
+    // copy module object to be accessible in callbacks
+    var self = this
+
+    // checkbox icon is added based on configuration
+    var checkbox = this.config.showCheckbox ? '▢ ' : ''
+
+    // styled wrapper of the todo list
+    var listWrapper = document.createElement('ul')
+    listWrapper.style.maxWidth = this.config.maxWidth + 'px'
+    listWrapper.style.paddingLeft = '0'
+    listWrapper.style.marginTop = '0'
+    listWrapper.style.listStyleType = 'none'
+    listWrapper.classList.add('small')
+
+    // for each entry add styled list items
+    if (this.list.length !== 0) {
+      this.list.forEach(function (element) {
+        // Get due date array
+        var taskDue = ''
+        if (self.config.showDueDate === true && element.dueDateTime != null) {
+          // timezone is returned as UTC
+          taskDue = Object.values(element.dueDateTime)
+          // converting time zone to browser provided timezone and formatting time according to configuration
+          taskDue = moment.utc(taskDue[0]).tz(Intl.DateTimeFormat().resolvedOptions().timeZone).format(self.config.dateFormat)
+        }
+        var listItem = document.createElement('li')
+        listItem.style.listStylePosition = 'inside'
+        listItem.style.whiteSpace = 'nowrap'
+        listItem.style.overflow = 'hidden'
+        listItem.style.textOverflow = 'ellipsis'
+        var listItemText = document.createTextNode(checkbox + taskDue + element.subject)
+        listItem.appendChild(listItemText)
+        // complete task when clicked on it
+        if (self.config.completeOnClick) {
+          listItem.onclick = function () {
+            self.sendSocketNotification('COMPLETE_TASK', { module: self.data.identifier, taskId: element.id, config: self.config })
+          }
+        }
+        listWrapper.appendChild(listItem)
+      })
+    } else {
+      // otherwise indicate that there are no list entries
+      listWrapper.innerHTML += '<li style="list-style-position:inside; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">' + this.translate('NO_ENTRIES') + '</li>'
+    }
+    return listWrapper
+  },
+
+  getTranslations: function () {
+    return {
+      en: 'translations/en.js',
+      de: 'translations/de.js'
+    }
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === ('FETCH_INFO_ERROR_' + this.config.id)) {
+      Log.error('An error occurred while retrieving the todo list from Microsoft To Do. Please check the logs.')
+      Log.error(payload.error)
+      Log.error(payload.errorDescription)
+      this.list = [{ subject: 'Error occurred: ' + payload.error + '. Check logs.' }]
+
+      this.updateDom()
+    }
+
+    if (notification === ('DATA_FETCHED_' + this.config.id)) {
+      this.list = payload
+      Log.info(this.name + ' received list of ' + this.list.length + ' items.')
+
+      // check if module should be hidden according to list size and the module's configuration
+      if (this.config.hideIfEmpty) {
+        if (this.list.length > 0) {
+          if (this.hidden) {
+            this.show()
+          }
+        } else {
+          if (!this.hidden) {
+            Log.info(this.name + ' hiding module according to \'hideIfEmpty\' configuration, since there are no tasks present in the list.')
+            this.hide()
+          }
+        }
+      }
+
+      this.updateDom()
+    }
+
+    if (notification === ('TASK_COMPLETED_' + this.config.id)) {
+      this.sendSocketNotification('FETCH_DATA', this.config)
+    }
+  },
+
+  start: function () {
+    // copy module object to be accessible in callbacks
+    var self = this
+
+    // start with empty list that shows loading indicator
+    self.list = [{ subject: this.translate('LOADING_ENTRIES') }]
+
+    // decide if a module should be shown if todo list is empty
+    if (self.config.hideIfEmpty === undefined) {
+      self.config.hideIfEmpty = false
+    }
+
+    // decide if a checkbox icon should be shown in front of each todo list item
+    if (self.config.showCheckbox === undefined) {
+      self.config.showCheckbox = true
+    }
+
+    // set default max module width
+    if (self.config.maxWidth === undefined) {
+      self.config.maxWidth = '450px'
+    }
+
+    // set default limit for number of tasks to be shown
+    if (self.config.itemLimit === undefined) {
+      self.config.itemLimit = '200'
+    }
+
+    // set default task completion on click to false
+    if (self.config.completeOnClick === undefined) {
+      self.config.completeOnClick = false
+    }
+
+    // decide if the task due date should be shown in front of each todo list item, if it exists
+    if (self.config.showDueDate === undefined) {
+      self.config.showDueDate = false
+    }
+
+    // format to display the due date
+    if (self.config.dateFormat === undefined) {
+      self.config.dateFormat = 'ddd MMM Do [ - ]'
+    }
+
+    // set default refresh interval to 60s
+    if (self.config.refreshSeconds === undefined) {
+      self.config.refreshSeconds = 60
+    }
+
+    // in case there are multiple instances of this module, ensure the responses from node_helper are mapped to the correct module
+    self.config.id = this.identifier
+
+    // update tasks every 60s
+    var refreshFunction = function () {
+      self.sendSocketNotification('FETCH_DATA', self.config)
+    }
+    refreshFunction()
+    setInterval(refreshFunction, self.config.refreshSeconds * 1000)
+  }
+
+})
---
+++ modules/MMM-MicrosoftToDo/node_helper.js
@@ -0,0 +1,181 @@
+/*
+  Node Helper module for MMM-MicrosoftToDo
+
+  Purpose: Microsoft's OAutht 2.0 Token API endpoint does not support CORS,
+  therefore we cannot make AJAX calls from the browser without disabling
+  webSecurity in Electron.
+*/
+var NodeHelper = require('node_helper')
+const request = require('request')
+
+module.exports = NodeHelper.create({
+
+  start: function () {
+    console.log(this.name + ' helper started ...')
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === 'FETCH_DATA') {
+      this.fetchData(payload)
+    } else if (notification === 'COMPLETE_TASK') {
+      this.completeTask(payload.taskId, payload.config)
+    } else {
+      console.log(this.name + ' - Did not process event: ' + notification)
+    }
+  },
+
+  completeTask: function (taskId, config) {
+    // copy context to be available inside callbacks
+    var self = this
+
+    var completeTaskUrl = 'https://graph.microsoft.com/beta/me/outlook/tasks/' + taskId + '/complete'
+
+    request.post({
+      url: completeTaskUrl,
+      headers: {
+        Authorization: 'Bearer ' + self.accessToken
+      }
+    }, function (error, response, body) {
+      if (error) {
+        console.error(self.name + ' - Error while requesting access token:')
+        console.error(error)
+        return
+      }
+
+      if (body && JSON.parse(body).error) {
+        console.error(self.name + ' - Error while completing tasks:')
+        console.error(JSON.parse(body).error)
+        self.sendSocketNotification('COMPLETE_TASK_ERROR', { error: JSON.parse(body).error.code, errorDescription: JSON.parse(body).error.message })
+        return
+      }
+
+      console.log(this.name + ' - Completed task with ID: ' + taskId)
+
+      // update front-end about success to trigger a refresh of the task list
+      self.sendSocketNotification('TASK_COMPLETED_' + config.id)
+    })
+  },
+
+  getTodos: function (config) {
+    // copy context to be available inside callbacks
+    var self = this
+
+    // get access token
+    var tokenUrl = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
+    var refreshToken = config.oauth2RefreshToken
+    var data = {
+      client_id: config.oauth2ClientId,
+      scope: 'offline_access user.read ' + (config.completeOnClick ? 'tasks.readwrite' : 'tasks.read'),
+      refresh_token: refreshToken,
+      grant_type: 'refresh_token',
+      client_secret: config.oauth2ClientSecret
+    }
+    request.post({
+      url: tokenUrl,
+      form: data
+    },
+    function (error, response, body) {
+      if (error) {
+        console.error(self.name + ' - Error while requesting access token:')
+        console.error(error)
+        return
+      }
+
+      if (body && JSON.parse(body).error) {
+        console.error(self.name + ' - Error while requesting access token:')
+        console.error(JSON.parse(body))
+
+        self.sendSocketNotification('FETCH_INFO_ERROR_' + config.id, { error: JSON.parse(body).error, errorDescription: JSON.parse(body).error_description })
+
+        return
+      }
+
+      const accessTokenJson = JSON.parse(body)
+      var accessToken = accessTokenJson.access_token
+      self.accessToken = accessToken
+
+      // get tasks
+      var _getTodos = function () {
+        var orderBy = (config.orderBy === 'subject' ? '&$orderby=subject' : '') + (config.orderBy === 'dueDate' ? '&$orderby=duedatetime/datetime' : '')
+        var listUrl = 'https://graph.microsoft.com/beta/me/outlook/taskFolders/' + config._listId + '/tasks?$select=subject,status,duedatetime&$top=' + config.itemLimit + '&$filter=status%20ne%20%27completed%27' + orderBy
+
+        request.get({
+          url: listUrl,
+          headers: {
+            Authorization: 'Bearer ' + accessToken
+          }
+        }, function (error, response, body) {
+          if (error) {
+            console.error(self.name + ' - Error while requesting access token:')
+            console.error(error)
+          }
+
+          if (body && JSON.parse(body).error) {
+            console.error(self.name + ' - Error while requesting tasks:')
+            console.error(JSON.parse(body).error)
+
+            self.sendSocketNotification('FETCH_INFO_ERROR_' + config.id, { error: JSON.parse(body).error.code, errorDescription: JSON.parse(body).error.message })
+
+            return
+          }
+
+          // send tasks to front-end
+          const tasksJson = JSON.parse(body)
+          self.sendSocketNotification('DATA_FETCHED_' + config.id, tasksJson.value)
+        })
+      }
+
+      // get ID of task folder
+      var taksFoldersUrl = 'https://graph.microsoft.com/beta/me/outlook/taskFolders/?$top=200'
+
+      request.get({
+        url: taksFoldersUrl,
+        headers: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      }, function (error, response, body) {
+        if (error) {
+          console.error(self.name + ' - Error while requesting task folders:')
+          console.error(error)
+
+          self.sendSocketNotification('FETCH_INFO_ERROR_' + config.id, { error: 'Error while requesting task folders', errorDescription: error })
+
+          return
+        }
+
+        // parse response from Microsoft
+        var list = JSON.parse(body)
+
+        // if list name was provided, retrieve its ID
+        if (config.listName !== undefined && config.listName !== '') {
+          list.value.forEach(element => element.name === config.listName ? (config._listId = element.id) : '')
+        } else if (config.listId !== undefined && config.listId !== '') {
+          // if list ID was provided copy it to internal list ID config and show deprecation warning
+          config._listId = config.listId
+          console.warn(self.name + ' - Warning, configuration parameter listId is deprecated, please use listName instead, otherwise the module will not work anymore in the future.')
+          // TODO: during the next release uncomment the following line to not show the todo list, but the error message instead
+          // self.sendSocketNotification('FETCH_INFO_ERROR_' + config.id, {
+          // error: 'Config param "listId" is deprecated, use "listName" instead',
+          // errorDescription: 'The configuration parameter listId is deprecated, please use listName instead. See https://github.com/thobach/MMM-MicrosoftToDo/blob/master/README.MD#installation' })
+        } else {
+          // otherwise identify the list ID of the default task list first
+          // set listID to default task list "Tasks"
+          list.value.forEach(element => element.isDefaultFolder ? (config._listId = element.id) : '')
+        }
+
+        if (config._listId !== undefined && config._listId !== '') {
+          // based on translated configuration data (listName -> listId), get tasks
+          _getTodos()
+        } else {
+          self.sendSocketNotification('FETCH_INFO_ERROR_' + config.id, { error: '"' + config.listName + '" task folder not found', errorDescription: 'The task folder "' + config.listName + '" could not be found.' })
+          console.error(self.name + ' - Error while requesting task folders: Could not find task folder ID for task folder name "' + config.listName + '", or could not find default folder in case no task folder name was provided.')
+        }
+      } // function callback for task folders
+      )
+    })
+  },
+
+  fetchData: function (config) {
+    this.getTodos(config)
+  }
+})
